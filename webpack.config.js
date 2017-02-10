162b92f99f2ba625c425ce28916e1666123094d6
Daniel Varela <dvarela@losaltosca.gov>
2017-02-10 15:01:58 -0800

    updated webpack to use babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
           test: /\.js$/,
           exclude: /(node_modules)/,
           loader: 'babel',
+          query: {
+            presets: ['es2015', 'react']
+          }
         },
       ]
     }
