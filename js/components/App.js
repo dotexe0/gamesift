de5b53bd9dae24c4fac1e8897a3aea5825b0e60c
Daniel Varela <dvarela@losaltosca.gov>
2017-02-10 13:04:33 -0800

    hello world

---
+++ js/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+    const hello = 'Hello World!';
+
+    return (
+        <div className="card">
+            {hello}
+        </div>
+    );
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
 
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+import App from './components/App';
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<App />, document.getElementById('app'))
+);
