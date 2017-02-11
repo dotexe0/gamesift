da5b442c5aa4523aa6ed03dde2e03b99d36159a6
Daniel Varela <dotexe0@gmail.com>
2017-02-11 15:12:39 -0800

    test

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function App() {
-  const greeting = 'Hello World!';
+  const greeting = '!!!Hello World!';
 
     return (
         <div className="card">
