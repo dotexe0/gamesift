bd4489589f6d8165ce6beaafdeeb1ed68fe93794
dotexe0 <dotexe0@gmail.com>
2017-04-20 16:51:13 -0700

    added bg img, landing pg

--- js/components/LandingPage.js
+++ js/components/LandingPage.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import {Link, hashHistory} from 'react-router';
-// import '../../css/LandingPage.css'
-
+// import backgroundImage from '../../assets/bgImg.jpg';
 export default class LandingPage extends Component  {
+
+    componentWillMount() {
+        document.body.style.background = 'url(../../assets/bgImg.jpg)';
+        document.body.style.opacity = '0.7';
+    }
+
+    componentWillUnmount() {
+        document.body.style.background = null;
+        document.body.style.opacity = null;
+    }
     render() {
+        const landingTitle = {
+            color: 'black',
+            marginTop: '150px',
+            fontFamily: 'Orbitron',
+            display: 'block',
+            letterSpacing: '10px'
+        }
+
+        const styledDiv = {
+            root: {
+                background: 'url(../../assets/bgImg.jpg)',
+                backgroundSize: 'cover',
+                overflow: 'hidden'
+            },
+            color: 'red'
+        }
         return(
-            <div>
-                <h1 className="webTitle"/>
-                <Link to="/home">Cinefilm</Link>
-                <h1>Hello World</h1>
+            <div style={styledDiv}>
+                <h1><Link to="/home" style={landingTitle}>Gamesift</Link></h1>
+                <h1>A video game synopsis search engine.</h1>
             </div>
             );
     }
